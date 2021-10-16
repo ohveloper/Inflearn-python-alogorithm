40d8f97921e5906c6d0d04c2fe94bd0474c4dad0
ohveloper <ohveloper@gmail.com>
2021-10-16 20:35:58 +0900

    숫자만추출/최초풀이

--- Inflearn/Section3_research_simulator/02_number_extraction.py
+++ Inflearn/Section3_research_simulator/02_number_extraction.py
@@ -5,3 +5,30 @@
 만약 “t0e0a1c2h0er”에서 숫자만 추출하면 0, 0, 1, 2, 0이고 이것을 자연수를 만들면 120이 됩니다.
 즉첫자리0은자연수화할때무시합니다. 출력은120를출력하고,다음줄에120 의 약수의 개수를 출력하면 됩니다.
 '''
+'''
+pseudo code
+1. 숫자만 필터하는 함수를 만든다
+2. 숫자만 필터해서 int로 만든다
+3. 약수의 개수를 리턴한
+'''
+
+def is_number(x):
+    res = ''
+    for i in x:
+        if i.isdigit() and int(i) != 0:
+            res += i
+    return int(res)
+
+def cnt_measure(num):
+    cnt = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            cnt += 1
+    return cnt
+
+print(cnt_measure(10))
+
+# print(is_number("hello0002233world"))
+# z = 'hello'
+# m = 'world'
+# print(z + m)
