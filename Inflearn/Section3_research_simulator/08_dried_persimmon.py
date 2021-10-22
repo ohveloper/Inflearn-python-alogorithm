5f4820bd79c3fcb1b4e381057ffb5996d8b5b33a
ohveloper <ohveloper@gmail.com>
2021-10-23 01:25:51 +0900

    곶감/되돌아보기

--- Inflearn/Section3_research_simulator/08_dried_persimmon.py
+++ Inflearn/Section3_research_simulator/08_dried_persimmon.py
@@ -1,9 +1,13 @@
 '''
+## 곶감, 모래시계 ##
 현수는 곳감을 만들기 위해 감을 깍아 마당에 말리고 있습니다. 현수의 마당은 N*N 격자판으 로 이루어져 있으며, 현수는 각 격자단위로 말리는 감의 수를 정합니다.
 그런데 해의 위치에 따라 특정위치의 감은 잘 마르지 않습니다. 그래서 현수는 격자의 행을 기준으로 왼쪽, 또는 오른쪽으로 회전시켜 위치를 변경해 모든 감이 잘 마르게 합니다.
 만약 회전명령 정보가 2 0 3이면 2번째 행을 왼쪽으로 3만큼 아래 그림처럼 회전시키는 명령 입니다.
 두번쨰 입력 정보가 1 이면 오른쪽으로 0이면 왼쪽으로
-
+'''
+'''
+작동은 시켰는데 코드가 정말 엉망이다..
+하다보면 더 나아지려나.. 좌우로 섞는 코드도 너무 어거지고 모래시계 모양으로 더하는것도 너무 어거지다...
 '''
 N = int(input("N : "))
 arr = [list(map(int, input("arr : ").split())) for _ in range(N)]
@@ -11,13 +15,15 @@ where = int(input("where? : "))
 lr = int(input("left==0, right==1 : "))
 M = int(input("how many move? : "))
 
-def dried_persimmon(N,arr,where,lr,M):
+
+def dried_persimmon(N, arr, where, lr, M):
     if lr == 0:
         arr[where - 1] = mix_list_left(N, M, arr[where - 1])
         return arr
     else:
         arr[where - 1] = mix_list_left(N, M, arr[where - 1])
         return arr
+
 
 def mix_list_left(N, M, a):
     b = list(a)
@@ -43,8 +49,9 @@ def mix_list_right(N, M, a):
     return a
 
 
-test = dried_persimmon(N,arr,where,lr,M)
+test = dried_persimmon(N, arr, where, lr, M)
 print(test)
+
 
 def last_dance(arr):
     s = 0
@@ -58,14 +65,13 @@ def last_dance(arr):
 
         s += num
         e -= num
-        print(s,e)
+        print(s, e)
         if s == N // 2:
             print("hello")
             num *= -1
 
-
-
     return res
+
 
 result = last_dance(test)
 print(result)
